1e2111c514eaf0b3ed6d79d313b9bfec0a973539
Marco-Borth <borthmarco@ku.edu>
2022-10-26 11:14:42 -0500

    modified starting files.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import $ from "jquery";
+import './css/App.css';
+//import './css/custom.css';
+import './css/cpadmin.css';
+import { CPAdmin } from './Dashboard/CPAdmin.js';
+import { LoginPage } from './Login/LoginPage.js';
+import { PasswordResetPage } from './Login/FindUser.js';
+import { HelpDeskPage } from './Login/HelpDesk';
+import { VerifyCodePanel } from './Login/VerifyCode.js';
+import { ChangePWPanel } from './Login/ChangePW.js';
+import { Footer } from './Reusables/Elements';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {HandleLogin()}
+      <Footer/>
     </div>
   );
+}
+
+function HandleLogin() {
+  var p = window.location.href.split('/');
+  if (p.length > 0) {
+    var nav = p[p.length - 1];
+
+    switch (nav) {
+      case "/":
+      case "":
+      case "login":
+        return (<LoginPage/>);
+
+      case "forgot":
+        return (<PasswordResetPage/>);
+
+      case "help":
+        return (<HelpDeskPage/>);
+
+      case "verify":
+        return (<VerifyCodePanel/>);
+
+      case "change-pw":
+        return (<ChangePWPanel/>);
+
+      default:
+        var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+
+        return (<CPAdmin isMobile={isMobile}/>);
+    }
+  }
+  return (<CPAdmin />);
 }
 
 export default App;
